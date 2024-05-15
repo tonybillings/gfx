cb0e35205dee6ef4ecfc45164243109c0d98f72d
Tony Billings <tony@tonysoft.com>
2024-05-15 13:56:44 -0700

    Update test validator for GitHub

--- _test/validation.go
+++ _test/validation.go
@@ -6,7 +6,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/tonybillings/gfx"
 	"image/color"
-	"os"
 	"sync/atomic"
 	"testing"
 )
@@ -128,13 +127,8 @@ func (v *SceneValidator) AddPixelSampler(getPixelPositionFunc func() (posX, posY
 }
 
 func (v *SceneValidator) Validate() {
+	SleepNFrames(5)
 	v.Reset()
-
-	if os.Getenv("GITHUB_ACTIONS") == "true" {
-		SleepNFrames(30)
-	} else {
-		SleepNFrames(5)
-	}
 
 	for _, e := range v.Errors {
 		v.t.Error(e)
